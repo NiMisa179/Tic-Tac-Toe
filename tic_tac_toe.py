7d2eefd497e436d8a43054e39f4e12acc7c12173
Nikos Misailidis <nikosmisailidis26@gmail.com>
2026-05-23 17:21:59 +0300

    value check

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,5 +3,5 @@ ans = input("S T A R T: Y/N?: ")
 
 while ans.upper() == "Y":
     row1 = int(input("Type the number of row you would like to choose: "))
-    while row1 < 1 or row1 > 4:
+    while row1 < 1 or row1 > 3:
         row1 = int(input("The number of row must be between 1 and 3 "))
