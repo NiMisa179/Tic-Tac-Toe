9c82b70cf63b04825f480937659f6b260a20c455
Nikos Misailidis <nikosmisailidis26@gmail.com>
2026-05-23 17:57:03 +0300

    added player 2

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,10 +1,14 @@
 print("Tic Tac Toe!")
 ans = input("S T A R T: Y/N?: ")
-tic = [" " for i in range(9)]
+tic = [ [" " for i in range(3)],
+        [" " for i in range(3)],
+        [" " for i in range(3)]
+      ]
 while ans.upper() == "Y":
 
-    for i in range(9 // 2):
+    for i in range((9 // 2) + 1):
 
+        print("Player 1: X")
         row1 = int(input("Type the number of row you would like to choose: "))
         while row1 < 1 or row1 > 3:
             row1 = int(input("The number of row must be between 1 and 3: "))
@@ -13,14 +17,35 @@ while ans.upper() == "Y":
         while col1 < 1 or col1 > 3:
             col1 = int(input("The number of col must be between 1 and 3: "))
 
-        pass
+        tic[row1-1][col1-1] = "x"
 
         print("+---+---+---+")
-        print("| " + tic[0].upper() + " | " + tic[1].upper() + " | " + tic[2].upper() + " |")
+        print("| " + tic[0][0].upper() + " | " + tic[0][1].upper() + " | " + tic[0][2].upper() + " |")
         print("+---+---+---+")
-        print("| " + tic[3].upper() + " | " + tic[4].upper() + " | " + tic[5].upper() + " |")
+        print("| " + tic[1][0].upper() + " | " + tic[1][1].upper() + " | " + tic[1][2].upper() + " |")
         print("+---+---+---+")
-        print("| " + tic[6].upper() + " | " + tic[7].upper() + " | " + tic[8].upper() + " |")
+        print("| " + tic[2][0].upper() + " | " + tic[2][1].upper() + " | " + tic[2][2].upper() + " |")
         print("+---+---+---+")
+
+        print("Player 2: O")
+        row2 = int(input("Type the number of row you would like to choose: "))
+        while row2 < 1 or row2 > 3:
+            row2 = int(input("The number of row must be between 1 and 3: "))
+
+        col2 = int(input("Type the number of column you would like to choose: "))
+        while col2 < 1 or col2 > 3:
+            col2 = int(input("The number of col must be between 1 and 3: "))
+
+        tic[row2 - 1][col2 - 1] = "o"
+
+        print("+---+---+---+")
+        print("| " + tic[0][0].upper() + " | " + tic[0][1].upper() + " | " + tic[0][2].upper() + " |")
+        print("+---+---+---+")
+        print("| " + tic[1][0].upper() + " | " + tic[1][1].upper() + " | " + tic[1][2].upper() + " |")
+        print("+---+---+---+")
+        print("| " + tic[2][0].upper() + " | " + tic[2][1].upper() + " | " + tic[2][2].upper() + " |")
+        print("+---+---+---+")
+
+        ans = " "
 
 
